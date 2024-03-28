a5a7144e409682067467e54c1c26d8a184decb2e
Morisson Marcel <@morissonmaciel>
2024-03-28 20:32:19 -0300

    Initial Commit

---
+++ XIAssistant/XIAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  XIAssistantApp.swift
+//  XIAssistant
+//
+//  Created by Morisson Marcel on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct XIAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
